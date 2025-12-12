9777679a9ba981507f5cd358f4907e460d2081e3
scotthovermn-create <scotthovermn@yahoo.com>
2025-12-11 22:02:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -218,11 +218,13 @@ while running:
         christmas_font_big   = pygame.font.Font("assets/JandaChristmasDoodles.ttf", 72)  # Title
         christmas_font_med   = pygame.font.Font("assets/MountainsOfChristmas.ttf", 56)  # Victory main lines
         christmas_font_small = pygame.font.Font("assets/JandaChristmasDoodles.ttf", 44)  # Victory small lines
+        christmas_font_tiny = pygame.font.Font("assets/to/MountainOfChristmas.ttf", 30) #Victory tiny lines 
     except:
         # Fallback if something goes wrong (will never happen if file is there)
         christmas_font_big   = pygame.font.SysFont("comicsansms", 72, bold=True, italic=True)
         christmas_font_med   = pygame.font.SysFont("comicsansms", 56, bold=True)
         christmas_font_small = pygame.font.SysFont("comicsansms", 44)
+        christmas_font_tiny = pygame.font.SysFont("comicsansms", 44)
 
     # "Merry Christmas" at the top — now REAL cursive!
     title_surf = christmas_font_med.render("Merry Christmas", True, GREEN)
@@ -291,7 +293,7 @@ while running:
             pygame.draw.circle(screen, c, (int(x), int(y)), random.randint(2,5))
 
        
-        restart = christmas_font_med.render("Blitzen thanks you for helping him save Christmas! Press R to play again!", True, WHITE)
+        restart = christmas_font_tiny.render("Blitzen thanks you for helping him save Christmas! Press R to play again!", True, WHITE)
         screen.blit(restart, restart.get_rect(center=(WIDTH // 2, HEIGHT - 100)))
         
         # OLD ELFIN VICTORY
